50f6577b7ed50638db26268712912d528a1cec9b
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2022-06-09 16:16:39 +0300

    Successfully set up ASP.NET Core MVC Application

---
+++ Common/ErrorMessageConstants/CommonErrorConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.ErrorMessageConstants
+{
+    public class CommonErrorConstants
+    {
+        public const string INTERVAL_ERROR = "{0} must be between {1} and {2}";
+    }
+}
