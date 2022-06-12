a844c0550a247acf4d0131ac972adccafc59dad9
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2022-06-12 12:05:06 +0300

    Implementing JS script for lineup validation.

---
+++ Web/wwwroot/js/lineup.js
@@ -0,0 +1,25 @@
+﻿let submitButtonElement = document.querySelector('button[type="submit"]');
+let checkboxElements = document.querySelectorAll('input');
+submitButtonElement.setAttribute('disabled', 'true');
+
+checkboxElements.forEach(cbe => cbe.addEventListener(
+    'change', () => {
+        let homeCheckboxElements = Array.from(document.querySelectorAll('.home input'));
+        let awayCheckboxElements = Array.from(document.querySelectorAll('.away input'));
+        checkStartingPLayers(homeCheckboxElements, awayCheckboxElements);
+
+    }));
+
+
+
+function checkStartingPLayers(homeCheckboxes, awayCheckboxes) {
+    let homeChecked = homeCheckboxes.filter(c => c.checked);
+    let awayChecked = awayCheckboxes.filter(c => c.checked);
+
+    if (awayChecked.length == 11 && homeChecked.length == 11) {
+        submitButtonElement.removeAttribute('disabled');
+    }
+    else {
+        submitButtonElement.setAttribute('disabled', 'true');
+    }
+}
