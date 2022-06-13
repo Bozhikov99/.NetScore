e62feb4770eb230ea67ea53de409b51e7e15b542
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2022-06-13 18:11:21 +0300

    Added a modal for editing player stats during a match.

---
+++ Web/wwwroot/js/match.js
@@ -0,0 +1,19 @@
+﻿let playerCardElements = document.querySelectorAll('.player-card');
+let modalContainerElement = document.querySelector('.modal-container');
+let closeElement = document.querySelector('#close');
+
+closeElement.addEventListener('click', () => {
+    modalContainerElement.setAttribute('hidden', true);
+});
+
+playerCardElements.forEach(pce => pce.addEventListener('mouseover', () => {
+    pce.classList.add('active-card');
+}));
+
+playerCardElements.forEach(pce => pce.addEventListener('mouseout', () => {
+    pce.classList.remove('active-card');
+}));
+
+playerCardElements.forEach(pce => pce.addEventListener('click', () => {
+    modalContainerElement.removeAttribute('hidden');
+}));
